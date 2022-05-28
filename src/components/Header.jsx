e8e24a12609b1ba7fee6c399cf6aeaca9cabb938
Fizzify <fizzyplayzoriginal5@gmail.com>
2022-05-28 21:42:08 +0800

    Add info about app with popup.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,21 @@
+import React from "react";
 import NoteIcon from "@mui/icons-material/Note";
+import InfoIcon from "@mui/icons-material/Info";
+import Typography from "@mui/material/Typography";
+import Popover from "@mui/material/Popover";
+import { Zoom, Tooltip } from "@mui/material";
+import {
+  usePopupState,
+  bindTrigger,
+  bindPopover,
+} from "material-ui-popup-state/hooks";
 
-function Header() {
+function Header(children) {
+  const popupState = usePopupState({
+    variant: "popover",
+    popupId: "demoPopover",
+  });
+
   return (
     <header>
       <h1>
@@ -9,6 +24,27 @@ function Header() {
           Notei
         </a>
       </h1>
+
+      <button {...bindTrigger(popupState)}>
+        <Zoom in={true}>
+          <Tooltip title="Info">
+            <InfoIcon />
+          </Tooltip>
+        </Zoom>
+      </button>
+      <Popover
+        {...bindPopover(popupState)}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+      >
+        <Typography style={{padding: "14px"}}>Notei is a note-taking app for organization. It has a sticky note feel to it as you can drag around the notes wherever you want.</Typography>
+      </Popover>
     </header>
   );
 }
